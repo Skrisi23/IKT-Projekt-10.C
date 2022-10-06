0b0f5b8b759d70c0dd3a712b37655d8e484117db
Somogyi Kristóf <SomogyiK@kkszki.local>
2022-10-06 13:18:27 +0200

    Kész

---
+++ Hev/hev.js
@@ -0,0 +1,16 @@
+function szamol()
+{
+    var indulo=document.getElementById("select1").value;
+    var érkezo=document.getElementById("select2").value;
+    
+    var eredmeny=indulo-érkezo;
+if (indulo==érkezo){
+
+    alert("Hiba:azonos megállokat választani");
+    
+}
+else{
+    alert("Menetidő:"+Math.abs(eredmeny)+"perc!")
+}
+
+}
