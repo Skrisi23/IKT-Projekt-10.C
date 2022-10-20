2947180e7ac7c998b9abe30333fb5d86e28eed0e
Somogyi Kristóf <Somogyik@KKSZKI.LOCAL>
2022-10-20 11:56:06 +0200

    3feladat kesz

---
+++ python/gyakorlo_1.py
@@ -0,0 +1,24 @@
+#1 feladat
+"""
+szam1 = int(input("kérném az első számot: "))
+szam2 = int(input("Kérném a második számot: "))
+szam2 = int(input("Kérném a harmadik számot: "))
+"""
+#2 feladat
+
+
+
+#3 feladat
+
+x = int(input("Kérek egy osztályzatot: "))
+
+if  x<50:
+    print("Elégtelen")
+elif 50 <=x< 60:
+    print("Elégséges")
+elif 60 <=x< 70:
+    print("Közepes")
+elif 70 <=x< 85:
+    print("Jó")
+elif x>=85:   
+    print("Jeles")
