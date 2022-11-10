3031125283790fd2d9c97d348c335ff2f1b9c6ed
Somogyi Kristóf <Somogyik@KKSZKI.LOCAL>
2022-11-10 12:56:19 +0100

    1feladatkesz

---
+++ python2feladat/1feladat.py
@@ -0,0 +1,13 @@
+a = input("Milyen napod van? (ÍRD jó/rosz) ")
+
+if a == "jó":
+    print("Örömmel hallom, további szép napot")
+
+if a == "rosz":
+    print("Sajnálattal hallom")
+
+elif a != "jó" and "rosz" :
+    print("Erre nincs válasz"  )
+
+       
+
