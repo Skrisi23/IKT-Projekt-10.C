cb01c4ba48c55933bb4bde60130a2b88c0a82d80
Skrisi23 <90719361+Skrisi23@users.noreply.github.com>
2022-11-16 18:12:43 +0100

    Add files via upload

---
+++ IKT Hazi/HF_2022_11_16.py
@@ -0,0 +1,7 @@
+szam1 = int(input("Kérem az első számot:  "))
+szam2 = int(input("Kérem a második számot:  "))
+
+print(szam1 + szam2, "A szám összege") 
+print(szam1 - szam2, "A szám külömbsége")
+print(szam1 * szam2, "A szám szorzata")
+print(szam1 / szam2, "A szám hányadosa")
