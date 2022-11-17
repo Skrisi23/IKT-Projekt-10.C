bc959333a2120ef77149104121faadfb9cbbefec
Somogyi Kristóf <Somogyik@KKSZKI.LOCAL>
2022-11-17 11:58:49 +0100

    1feladatkesz

---
+++ oraimunka_2022_11_17/1feladat.py
@@ -0,0 +1,12 @@
+#1 feladat
+"""
+szam = 1
+while szam <= 20:
+    if szam % 2 == 1:
+        print(szam)
+        szam += 1
+    elif szam % 2 == 0:
+        print(szam)
+        szam += 1        
+"""
+#------------------------------------------------
